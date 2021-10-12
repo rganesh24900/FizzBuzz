1827d7b0547bfc0d39e774df377b3064b974e0dc
Ganesh <rganesh802@gmail.com>
2021-10-12 18:00:53 +0530

    FizzBuzz

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int n = 20 ;
+    int arr[] = new int[n];
+    for (int i=1;i<n;i++){
+
+
+        if(i%3==0 && i%5==0){
+            System.out.print("FizzBuzz");
+        }
+        else if(i%3==0){
+           System.out.println("Fizz");
+        }
+        else if(i%5==0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(i);
+        }
+
+
+    }
+
+    }
+}
